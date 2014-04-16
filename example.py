bea5d2c35de25466294cd78e9135062612b9ccb0
blob8108 <spaaam42@gmail.com>
2014-04-16 19:12:46 +0100

    More interesting tests

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 
 import blockext
 from blockext import *
@@ -20,19 +21,55 @@ def play_note(note):
     print("DING {note}".format(note=note))
     time.sleep(2)
 
-menu("pizza", ["tomato", "cheese", "hawaii"])
+menu("pizza", ["tomato", "cheese", "hawaii", "nothing",
+               "spinach and cauliflower", "empty return value",
+               "cheese and tomato", "fancy",
+               "ü",
+               "/",
+               ])
 
 @reporter("colour of %m.pizza flavour pizza")
 def pizza_colour(pizza="tomato"):
     return {
         "tomato": "red",
         "cheese": "yellow",
-        "hawaii": "orange",
+        "cheese and tomato": "YELLOW",
+        "hawaii": "orange AND BLUE",
+        "spinach and cauliflower": "GREEN ü",
+        "empty return value": "",
+        "nothing": "",
+        "/": "SLASH",
+        u"ü": "unicode",
+        u"fancy": "❤☀☆☂",
     }[pizza]
+
+@reporter("spaaace")
+def getSpaaace():
+    return "space space space space space!"
 
 @reporter("id %s")
 def id(text):
+    """Tests strings can get passed from Snap! to Python and back."""
+    print(text)
     return text
+
+@command("set number to %n% units")
+def percent(number=42):
+    print(number)
+
+foo = None
+
+@command("set foo to %s")
+def set_foo(value=''):
+    global foo
+    foo = value
+
+@reporter("foo")
+def get_foo():
+    return foo
+
+@command("ü")
+def x(): pass
 
 @problem
 def my_problem():
